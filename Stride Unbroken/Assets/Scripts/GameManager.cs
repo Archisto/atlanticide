2de1f8f42265e01299719a984d88baa206bc9f1c
Lauri Kosonen <lauri.kosonen@cs.tamk.fi>
2018-09-03 21:43:27 +0300

    Initial commit

---
+++ Stride Unbroken/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StrideUnbroken
+{
+    public class GameManager : MonoBehaviour
+    {
+        #region Statics
+        private static GameManager instance;
+        public static GameManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<GameManager>();
+
+                    if (instance == null)
+                    {
+                        Debug.LogError("A GameManager object could not be found in the scene.");
+                    }
+                }
+
+                return instance;
+            }
+        }
+        #endregion Statics
+
+        private GameUI UI;
+
+        public bool GameOver
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Initializes the game.
+        /// </summary>
+        void Awake()
+        {
+            UI = FindObjectOfType<GameUI>();
+            if (UI == null)
+            {
+                Debug.LogError("GameUI object could not be found in the scene.");
+            }
+        }
+    }
+}
---
+++ Stride Unbroken/Assets/Scripts/InputManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StrideUnbroken
+{
+    public class InputManager : MonoBehaviour
+    {
+        private const string HorizontalKey = "Horizontal";
+        private const string VerticalKey = "Vertical";
+        private const string ActionKey = "Action";
+
+        [SerializeField]
+        private CameraController _camera;
+
+        /// <summary>
+        /// Initializes the object.
+        /// </summary>
+        private void Start()
+        {
+
+        }
+
+        /// <summary>
+        /// Updates the object once per frame.
+        /// </summary>
+        private void Update()
+        {
+            CheckInput();
+        }
+
+        private void CheckInput()
+        {
+            // Rotating the big cube
+            Vector3 direction = new Vector3(Input.GetAxisRaw(HorizontalKey), Input.GetAxisRaw(VerticalKey));
+
+            if (direction != Vector3.zero)
+            {
+                
+            }
+        }
+    }
+}
