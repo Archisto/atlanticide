020703cdbe8e0c54a62aecb73a4992f0adf26ba1
Valtteri <valtteri.hasia@cs.tamk.fi>
2018-10-12 11:39:53 +0300

    EnemyCharacter related changes
    EnemyCharacter script & prefab, Shield script
    Modified DamageDealre

---
+++ Atlanticide/Assets/Scripts/Enemy/Insect.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Atlanticide
+{
+
+    public class Insect : EnemyCharacter
+    {
+
+        #region start methods overwritten
+
+        protected override void startNoTarget()
+        {
+            base.startNoTarget();
+        }
+
+        protected override void startTargetOn()
+        {
+            base.startTargetOn();
+        }
+
+        protected override void startAttack()
+        {
+            base.startAttack();
+        }
+
+        #endregion
+
+        #region run methods overwritten
+
+        protected override void noTarget()
+        {
+            base.noTarget();
+        }
+
+        protected override void targetOn()
+        {
+            base.targetOn();
+        }
+
+        protected override void attack()
+        {
+            base.attack();
+        }
+
+        #endregion
+
+        #region end methods overwritten
+
+        protected override void endNoTarget()
+        {
+            base.endNoTarget();
+            
+        }
+
+        protected override void endTargetOn()
+        {
+            base.endTargetOn();
+        }
+
+        protected override void endAttack()
+        {
+            base.endAttack();
+        }
+
+        #endregion
+
+    }
+}
