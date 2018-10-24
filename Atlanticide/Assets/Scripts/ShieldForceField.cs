cd2d654bd326c6ac5abbeb3169be7d4d94acbf5c
AntoniusMaximus80 <toni.mutanen@cs.tamk.fi>
2018-10-24 21:49:20 +0300

    Shield's force field now works as intended.

---
+++ Atlanticide/Assets/Scripts/ShieldForceField.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Atlanticide
+{
+    public class ShieldForceField : MonoBehaviour
+    {
+        public GameObject _shieldForceField;
+        private Animator _shieldAnimator;
+
+        private void Start()
+        {
+            _shieldAnimator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_shieldAnimator.GetCurrentAnimatorStateInfo(0).IsName("Shield Open Idle"))
+            {
+                _shieldForceField.SetActive(true);
+            } else
+            {
+                _shieldForceField.SetActive(false);
+            }
+        }
+    }
+}
