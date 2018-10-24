d8d4c768c0b26fa03c3437cb753d397ba2e54786
AntoniusMaximus80 <toni.mutanen@cs.tamk.fi>
2018-10-24 18:46:15 +0300

    Non-functional shield force field behaviour attempt.

---
+++ Atlanticide/Assets/Scripts/Behaviour/ChildGameObjectFetcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Atlanticide
+{
+    public class ChildGameObjectFetcher : MonoBehaviour
+    {
+        public GameObject _forceFieldGameObject;
+
+        public GameObject GetForceField()
+        {
+            return _forceFieldGameObject;
+        }
+    }
+}
