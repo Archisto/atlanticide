c3e0f20dc1437bf78e5ea97ab71244d76f18f2d0
AntoniusMaximus80 <toni.mutanen@cs.tamk.fi>
2018-11-30 09:36:21 +0200

    More work on destructable buildings.

--- Atlanticide/Assets/Scripts/Destructables/DestructibleGameObject.cs
+++ Atlanticide/Assets/Scripts/Destructables/DestructibleGameObject.cs
@@ -16,28 +16,41 @@ namespace Atlanticide
         {
             base.Start();
             _prefabSpawners = GetComponentsInChildren<PrefabSpawner>(true);
-            Debug.Log("PrefabSpawners found: " + _prefabSpawners.Length);
+            //Debug.Log("PrefabSpawners found: " + _prefabSpawners.Length);
             _meshRenderer = GetComponent<MeshRenderer>();
             _collider = GetComponent<Collider>();
             _particleSystems = GetComponentsInChildren<ParticleSystem>(true);
-            Debug.Log("ParticleSystems found: " + _particleSystems.Length);
+            //Debug.Log("ParticleSystems found: " + _particleSystems.Length);
         }
 
         public void Destruction()
         {
             _toughnessLeft = 0f;
-            foreach (PrefabSpawner prefabSpawner in _prefabSpawners)
-            {
-                prefabSpawner.gameObject.SetActive(true);
+            if (_prefabSpawners.Length != 0) {
+                foreach (PrefabSpawner prefabSpawner in _prefabSpawners)
+                {
+                    prefabSpawner.gameObject.SetActive(true);
+                }
             }
-            foreach (ParticleSystem prefabSpawner in _particleSystems)
-            {
-                prefabSpawner.gameObject.SetActive(true);
+            if (_particleSystems.Length != 0) {
+                foreach (ParticleSystem prefabSpawner in _particleSystems)
+                {
+                    prefabSpawner.gameObject.SetActive(true);
+                }
             }
-            _meshRenderer.material.color = new Color(0f, 0f, 0f, 0f);
+            _meshRenderer.material.color = new Color(1f, 1f, 1f, 0f);
             _collider.enabled = false;
-            SFXPlayer.Instance.Play(Sound.Cyclops_Exploding, volumeFactor: 0.3f);
+            //SFXPlayer.Instance.Play(Sound.Cyclops_Exploding, volumeFactor: 0.3f);
             Destroy(gameObject, destroyDelay);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.gameObject.layer == 16)
+            {
+                Debug.Log("Destruction()");
+                Destruction();
+            }
         }
     }
 }
