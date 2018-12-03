470831f43287e44424b2b513cfaa25cf0ef71a21
AntoniusMaximus80 <toni.mutanen@cs.tamk.fi>
2018-12-03 19:03:17 +0200

    Worked on level elevator and level back wall pieces.

---
+++ Atlanticide/Assets/Scripts/LevelObjects/LevelElevator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Atlanticide
+{
+    public class LevelElevator : MonoBehaviour
+    {
+        private PlayerCharacter[] playerCharacters;
+
+        public GameObject elevatorGear;
+
+        public float elevatorSpeed,
+            gearRotationSpeed;
+
+        private float gearRotationAngle;
+
+        public bool raiseElevator = false;
+
+        // Use this for initialization
+        void Start()
+        {
+            playerCharacters = FindObjectsOfType<PlayerCharacter>();
+            gearRotationAngle = Time.deltaTime * gearRotationSpeed;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (raiseElevator)
+            {
+                Vector3 translateVector3 = new Vector3(0f, 0f, Time.deltaTime * elevatorSpeed);
+                transform.Translate(translateVector3);
+                elevatorGear.transform.Rotate(Vector3.left, gearRotationAngle);
+            }
+        }
+
+        public void RaiseElevator()
+        {
+            raiseElevator = true;
+            // Freeze players here!
+        }
+    }
+}
